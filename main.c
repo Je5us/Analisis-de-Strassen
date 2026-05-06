777700d4adac4b3d8787ae0e4ce7f022119ab2b5
Je5us <jperezvil@unsa.edu.pe>
2026-05-05 23:37:06 -0500

    implementacion de datos aleatorios y medicion de tiempo

--- main.c
+++ main.c
@@ -1,32 +1,69 @@
 #include "multiplicacion_estandar.h"
 #include "strassen.h"
 #include "stdio.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main() {
-	int n = 4; 
+static long long elapsedNanoseconds(struct timespec start, struct timespec end) {
+    long long seconds = (long long)(end.tv_sec - start.tv_sec);
+    long long nanoseconds = (long long)(end.tv_nsec - start.tv_nsec);
+    return seconds * 1000000000LL + nanoseconds;
+}
 
-    int **A = crear_matriz(n);
-    int **B = crear_matriz(n);
-	int **C = crear_matriz(n);
-    int **D = crear_matriz(n);
-	
+//generar matrices con valores aleatorios
+void generar_matriz_aleatoria(int** M, int n, int maxValue){
 	for(int i = 0; i < n; i++){
 		for(int j = 0; j < n; j++){
-			A[i][j] = i * n + j;
-			B[i][j] = i * n + j;
-		}
+			M[i][j] = rand() % maxValue;
+		}	
 	}
+}
 
+static double medicion_estandard(int** A, int** B,int** C, int n){
+	struct timespec start;
+	struct timespec end;
+
+	clock_gettime(CLOCK_MONOTONIC, &start);
 	multiplicacion_estandar(A, B, C, n);
-	mostrar(C, n);
-	printf("Strassen: \n");
-    strassen(A, B, D, n);
-	mostrar(D, n);
-	
-    liberar_matriz(A, n);
-    liberar_matriz(B, n);
-	liberar_matriz(C, n);
-    liberar_matriz(D, n);
+	clock_gettime(CLOCK_MONOTONIC, &end);
 
-    return 0;
+	return (double)elapsedNanoseconds(start, end) / 1000.0;
+}
+
+static double medicion_strassen(int** A, int** B, int** C, int n){
+	struct timespec start;
+	struct timespec end;
+
+	clock_gettime(CLOCK_MONOTONIC, &start);
+	strassen(A, B, C, n);
+	clock_gettime(CLOCK_MONOTONIC, &end);
+
+	return (double)elapsedNanoseconds(start, end) / 1000.0;
+}
+
+int main() {
+	srand((unsigned int)time(NULL));
+	printf("#n\tEstandar\tStrassen:\n");
+	
+	for(int n = 2; n <= 512; n *= 2){ 
+    	int **A = crear_matriz(n);
+    	int **B = crear_matriz(n);
+		int **C = crear_matriz(n);
+    	int **D = crear_matriz(n);
+
+		generar_matriz_aleatoria(A, n, 1000);
+		generar_matriz_aleatoria(B, n, 1000);
+	
+		double Estandartime = medicion_estandard(A, B, C, n);
+		double Strassentime = medicion_strassen(A, B, D, n);
+
+		printf("%d\t%.3f\t%.3f\n", n, Estandartime, Strassentime);
+
+		liberar_matriz(A, n);
+		liberar_matriz(B, n);
+		liberar_matriz(C, n);
+		liberar_matriz(D, n);
+	}
+	return 0;
 }
